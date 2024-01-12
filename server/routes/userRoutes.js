6ad3ce32054162fdb9dae339b59e9aec339f5be5
Avery Myers <averyjmyers@hotmail.com>
2024-01-12 12:22:40 -0800

    working user creation route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -20,6 +20,33 @@ router.get('/all', async (req, res) => {
 
 //create user
 
+router.post('/signup', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        });
+
+        // Check if an account with this email already exists
+        if (userData) {
+            res.status(400).json({ message: "An account with this email already exists!" });
+        } else {
+            // Create a new user
+            await User.create({
+                email: req.body.email, 
+                password: req.body.password 
+            });
+
+            console.log(`Welcome ${req.body.email}!`);
+            res.status(200).json({ message: `Welcome ${req.body.email}!` });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
 
 
 router.post('/login', async (req, res) => {
